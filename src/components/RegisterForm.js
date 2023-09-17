efdc7aadcaa8d40d0bc40a178375a212d807e476
builde7b0b <ggives3@gmail.com>
2023-09-17 01:25:48 -0700

    refactor to add notifications and redirector after registration

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom'
 import './RegisterForm.css'; 
 import { registerUser } from '../services/api';
 
 
-const registerUserF = (e) => {
-    e.preventDefault();
-    const email = e.target.elements[0].value;
-    const password = e.target.elements[1].value;
-    const userData = { email, password };
-    registerUser(userData);
-}
+
 function RegisterForm() {
+    const [isRegistered, setIsRegistered] = useState(false);
+    const navigate = useNavigate();
+
+    const registerUserF = async (e) => {
+        e.preventDefault();
+        const email = e.target.elements[0].value;
+        const password = e.target.elements[1].value;
+        const userData = { email, password };
+
+        try {
+            const response = await registerUser(userData);
+            if (response.status === 200) {
+                setIsRegistered(true);
+                 // Notify the user 
+            // alert('You have successfully registered! Redirecting to Login');
+
+            // navigate('/login');
+            }
+        } catch (error) {
+            console.error("Registration failed:", error);
+        }
+        registerUser(userData);
+    }
+
+    useEffect(() => {
+        if (isRegistered) {
+            // Notify the user 
+            alert('You have successfully registered! Redirecting to Login');
+
+            navigate('/login');
+        }
+    }, [isRegistered, navigate]);
+
     return (
         <div className="register-form">
             <h1>Register</h1>
